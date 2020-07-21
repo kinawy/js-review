75551bc5193e1b3d7e8bc098290a64ff122757b9
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-20 20:42:45 -0400

    [JS] Finish the reverse string function

--- problems.js
+++ problems.js
@@ -1,7 +1,9 @@
 // Write a function that checks if an element is inside of an array
 
 function isInside (array, ele) {
+    // Create variable for the index of our element
     let indexArray = array.indexOf(ele);
+    // Create a condition checking if our array is inside of our array, if it is inside, its not at index -1 and will return true
     if (indexArray != -1){
         return true;
         } else {
@@ -18,7 +20,7 @@ console.log(isInside(['taylor', 'rome', 'adam'], 'mom'));
 // Then I used the reverse method to reverse the array
 // Then I used the join method to rejoin all of the seperated characters inside of the array into a backwards word.
 function reverseStr (string) {
-    // 
+    // this function just returns the string split with no space so it goes by character in an array with the split method, then using the reverse array method it reverses it, then rejoins with the join array method to be the reversed string.
         return string.split("").reverse().join("");
            
     
